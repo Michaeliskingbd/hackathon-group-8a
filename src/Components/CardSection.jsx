45437d582db6e677e2e7c71a80bcfe6c1b1e4019
deminuel <ayomidenuel21@gmail.com>
2025-10-24 03:24:22 -0700

    Instructors

---
+++ src/Components/CardSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const CardSection = ({img, course, name, about, contact}) => {
+  return (
+    <article className=''>
+      <div className='border-2 border-y-gray-300 rounded-2xl w-[350px] h-[400px] '> 
+      <img className='rounded-2xl w-[400px] h-[200px]' src={img} alt="" />
+      <div className='p-4 space-y-2'>
+      <h2 className='font-semibold'>{course}</h2>
+      <h1 className='font-semibold text-2xl'>{name}</h1>
+      <span className='w-[70px]'>{about}</span>
+      <p className='text-orange-400 hover:cursor-pointer'><i class="fa-solid fa-caret-right"></i>{contact}</p>
+      </div>
+      </div>
+    </article>
+  )
+}
+
+export default CardSection;
---
+++ src/Components/KardSection.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import CardSection from './CardSection';
+
+const KardSection = () => {
+  return (
+    <section className='bg-gray-200  '>
+        <div className=' grid lg:grid-cols-3 gap-5 grid-cols-1 lg:mx-28 mx-0'>
+        <CardSection
+        img="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2021/03/instructor-02-free-img.jpg" 
+        course="HTML5/CSS3 instructor"
+        name="MRS. Deborah Holmes"
+        about="Duis aute irure dolor in velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat voluptas"
+        contact="View profile" />
+
+        <CardSection  
+        img="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/02/instructor-04-free-img.jpg"
+        course="Digital Marketing" 
+        name="Bruce Stevens"
+        about="Duis aute irure dolor in velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat voluptas."
+        contact="View Profile" />
+
+        <CardSection 
+        img="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/02/instructor-03-free-img.jpg"
+        course="WordPress"
+        name="Michelle Baker"
+        about="Duis aute irure dolor in velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat voluptas."
+        contact="View Profile"/>
+
+        <CardSection 
+        img="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/02/instructor-05-free-img.jpg"
+        course="JavaScript"
+        name="Paul Santos"
+        about="Duis aute irure dolor in velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat voluptas."
+        contact="View Profile"/>
+
+        <CardSection 
+        img="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/03/instructor-01-free-img.jpg"
+        course="UI/UX"
+        name="Dana Caroll"
+        about="Duis aute irure dolor in velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat voluptas."
+        contact="View Profile"/>
+
+        <CardSection 
+        img="https://websitedemos.net/online-courses-02/wp-content/uploads/sites/542/2020/03/instructor-06-free-img.jpg"
+        course="Backend"
+        name="Scott Valdes"
+        about="Duis aute irure dolor in velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat voluptas."
+        contact="View Profile"/>
+
+    </div>
+    </section>
+  )
+}
+
+export default KardSection;
