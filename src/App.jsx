79ce2c4b7dd1fbbd18d6a052687e9ca63255a0db
boyesky <ogunrindeboye@gmail.com>
2025-11-03 07:16:53 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import AllCou from "./components/AllCou";
 import Courses2A from "./components/Courses2A";
+import Courses from "./Courses";
+import Instructors from "./pages/Instructors";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" index element={<Home />} />
-      {/* <Route path="/instructors" element={<Instructors />} />
-      <Route path="/aboutPage" elements={<About />} /> */}
+      <Route path="/instructors" element={<Instructors />} />
+      {/*<Route path="/aboutPage" elements={<About />} /> */}
       <Route path="/All" element={<AllCou />} />
       <Route path="/cour2" element={<Courses2A />} />
     </Routes>
-
   );
 };
 
