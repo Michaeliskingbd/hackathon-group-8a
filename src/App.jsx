5694a77ced3686b88f5c84220f1c78dd44b5daf8
deminuel <ayomidenuel21@gmail.com>
2025-11-12 05:14:29 -0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -20,58 +20,35 @@ import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <div></div>
  
      
 
-  //  <Routes>
-  //   <Route path='/instructor' element={<Instructors />} />
-  //   <Route path='/contact' element={<Contact />}/>
+   <Routes>
+    <Route path='/instructor' element={<Instructors />} />
+    <Route path='/contact' element={<Contact />}/>
 
-  //   <Routes>
-  //     <Route path="/" index element={<Home />} />
-  //     <Route path="/AllCourses" index element={<AllCourses />} />
-  //     {/* <Route path="/instructors" element={<Instructors />} /> */}
-  //     {/*  <Route path="/aboutPage" elements={<About />} /> */}
-  //     <Route path="/All" element={<AllCou />} />
-
-  //     {/* <Route path="/pricingPage" element={<PricingPage />} /> */}
-  //     <Route path="/Cou2" element={<Courses2A />} />
-  //     <Route path="/Cou3" element={<Courses3A />} />
-
-  //     //Extended pages
-  //     <Route path="/Ext" element={<CrsExtend />} />
-  //     <Route path="/Hattb" element={<HtmlAttb />} />
-  //     <Route path="/Atag" element={<AnaTag />} />
-  //     <Route path="/SelCss" element={<SelCss />} />
-  //     <Route path="/UiuxExt" element={<UiuxExt />} />
-  //     <Route path="/UiuxExt2" element={<UiuxExt2 />} />
-  //     <Route path="/UiuxExt3" element={<UiuxExt3 />} />
-  //     <Route path="/UiuxExt4" element={<UiuxExt4 />} />
-  //     <Route path="/Csec" element={<Csec />} />
-  //     <Route path="/Csec2" element={<Csec2 />} />
-  //     <Route path="/Csec3" element={<Csec3 />} />
-  //     <Route path="/Csec4" element={<Csec4 />} />
-  //   </Routes>
+    
+  
+   
 
 
 
-  //   {/* //Dashboard */}
-  //    <Route path="/" element={<Layout />}>
-  //  <Route index element={<Home />} /> 
-  //  <Route path='/dashboard' element={<Dashboard />} />
-  //  <Route path='/DashboardProfile' element={<Profile />} /> 
-  //   <Route path='/DashboardResources' element={<Resources />} /> 
-  //    <Route path='/DashboardCourses' element={<Courses />} /> 
-  //    <Route path='/DashboardJob' element={<JobPage />} /> 
-  //     <Route path='/form' element={<Form />} /> 
-  //     <Route path='/notice' element={<NoticeBoard />} /> 
-  //     <Route path='/assignment' element={<Assignment />} />
+    {/* //Dashboard */}
+     <Route path="/" element={<Layout />}>
+   <Route index element={<Home />} /> 
+   <Route path='/dashboard' element={<Dashboard />} />
+   <Route path='/DashboardProfile' element={<Profile />} /> 
+    <Route path='/DashboardResources' element={<Resources />} /> 
+     <Route path='/DashboardCourses' element={<Courses />} /> 
+     <Route path='/DashboardJob' element={<JobPage />} /> 
+      <Route path='/form' element={<Form />} /> 
+      <Route path='/notice' element={<NoticeBoard />} /> 
+      <Route path='/assignment' element={<Assignment />} />
      
   
-  //  </Route>
+   </Route>
 
-  //  </Routes> 
+   </Routes> 
    
       
      
